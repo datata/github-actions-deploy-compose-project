21d460263c817256f611e3569791fbb7da442a96
datata <danitt15@gmail.com>
2022-02-12 22:34:53 +0100

    feat: create contact endpoint

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const port = process.env.PORT || 3307;
 
 app.get('/', (req, res) => res.send('Mi primer deploy automatizado con pipelines'));
 app.get('/projects', (req, res) => res.send('Node devops'));
+app.get('/contact', (req, res) => res.send('Contact'));
+
 
 
 app.listen(port, () => console.log(port));
