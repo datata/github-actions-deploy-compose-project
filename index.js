d0f44d6ab5a2f10adca5752a91833de9f6ab7cb7
datata <danitt15@gmail.com>
2022-02-12 14:47:24 +0100

    feat: add new projects endpoint

--- index.js
+++ index.js
@@ -5,5 +5,7 @@ require('dotenv').config();
 const port = process.env.PORT || 3307;
 
 app.get('/', (req, res) => res.send('Mi primer deploy automatizado con pipelines'));
+app.get('/projects', (req, res) => res.send('Node devops'));
+
 
 app.listen(port, () => console.log(port));
